08aa7dcb65125e1bfe412f708b091d7ea0bc8eee
matt.eland@gmail.com <matt.eland@gmail.com>
2017-11-08 11:34:11 -0500

    6

--- MattEland.Common/Providers/IHasContainer.cs
+++ MattEland.Common/Providers/IHasContainer.cs
@@ -4,7 +4,7 @@
 // Created on:      09/01/2015 at 11:39 PM
 // Last Modified:   09/01/2015 at 11:39 PM
 // 
-// Last Modified by: Matt Eland 12345
+// Last Modified by: Matt Eland 123456
 // ---------------------------------------------------------
 
 using MattEland.Common.Annotations;
